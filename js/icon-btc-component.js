a8dfbbb1b1bfe1860c50bafcc5128825588fd5ff
omarmauricio.juarez.contractor <omarmauricio.juarez.contractor@bbva.com>
2020-05-06 16:19:14 -0500

    Se agrega componente para el icono de BitCoin.

---
+++ js/icon-btc-component.js
@@ -0,0 +1,7 @@
+class IconBtcComponent extends Polymer.Element {
+    static get is() {
+        return "icon-btc-component";
+    }
+}
+
+window.customElements.define(IconBtcComponent.is, IconBtcComponent);
